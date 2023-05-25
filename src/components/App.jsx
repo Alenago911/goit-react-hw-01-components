ab91ea3c9ee21361d1427c78d7d563d61d0b99cd
Alenago911 <alena.chernyavka@gmail.com>
2023-05-25 11:31:41 +0300

    дз

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,8 +5,8 @@ import {Statistics} from "./Statistics/Statistics.js";
 import data from "../data/data.json";
 import {FriendList} from "./FriendList/FriendListItem.js";
 import friends from "../data/friends.json";
-import {transactions} from "./TransactionHistory/TransactionHistory.js";
-import TransactionHistory from "../data/transactions.json";
+import {TransactionHistory} from "./TransactionHistory/TransactionHistory.js";
+import transactions from "../data/transactions.json";
 
 
 export const App = () => {
