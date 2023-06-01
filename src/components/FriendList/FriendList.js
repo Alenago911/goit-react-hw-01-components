4637207acdfed1885421ffe1945bcab2bac2169e
Alenago911 <alena.chernyavka@gmail.com>
2023-06-01 14:00:26 +0300

    нове

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,22 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import css from './FriendList.module.css';
+import FriendListItem from './FriendListItem';
 
-const FriendListItem = ({ avatar, name, isOnline }) => {
-  return (
-    <li className={css.item}>
-      <span className={isOnline ? css.statusOnline : css.statusOffline}></span>
-      <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
-      <p className={css.name}>{name}</p>
-    </li>
-  );
-};
-
-FriendListItem.propTypes = {
-  avatar: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  isOnline: PropTypes.bool.isRequired,
-};
 
 export const FriendList = ({ friends }) => (
   <div>
@@ -30,3 +15,16 @@ export const FriendList = ({ friends }) => (
     ))}
   </div>
 );
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
+
+
